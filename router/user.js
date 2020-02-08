429d61a6e0261cd4d3821aef8419771d1ae088b3
abirM-H <56075865+abirM-H@users.noreply.github.com>
2020-02-08 08:21:37 -0800

    Add files via upload

---
+++ router/user.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const router = express.Router();
+
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const passport = require("passport");
+const config = require("config");
+const User = require("../models/user");
+
+// register
+router.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
+
+  User.findOne({ email }).then(user => {
+    if (user) {
+      return res.json({ error: "user already exist" });
+    }
+    const newUser = new User({
+      name,
+      email,
+      password,
+    });
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
+
+        newUser.password = hash;
+        newUser
+          .save()
+          .then(user => res.json(user))
+          .catch(err => console.log(err));
+      });
+    });
+  });
+});
+
+// login
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  User.findOne({ email }).then(user => {
+    const { id, email, name } = user;
+    if (!user) {
+      return res.json({ error: "email is not valid" });
+    }
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        const payload = { id, email, name };
+        jwt.sign(
+          payload,
+          config.get("secretkey"),
+          { expiresIn: 3600 },
+          (err, token) => {
+            if (err) throw err;
+            res.json({ token: "Bearer " + token });
+          }
+        );
+      } else {
+        res.json({ error: "password incorrect" });
+      }
+    });
+  });
+});
+
+// validation
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json(req.user);
+  }
+);
+
+router.put("/update", (req, res) => {
+  child=req.body
+  Contact.findOneAndUpdate(req.params.email, { $push: { childs: child} })
+    .then(user => res.send(user))
+    .catch(err => res.send(err));
+});
+module.exports = router;
